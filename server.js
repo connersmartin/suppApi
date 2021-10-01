d3a6ede1bb8e6cfd95c8581d2d5ea8b582568cfc
Conner Martin <connersmartin@gmail.com>
2021-09-30 22:00:42 -0700

    attempt to play with cors part 6

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.ALLOW_DOMAIN); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   next();
 });
 
