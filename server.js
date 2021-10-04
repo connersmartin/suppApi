a45b7874e44ebef0d4702053053dd9cad08a2a10
Conner Martin <connersmartin@gmail.com>
2021-10-03 20:41:10 -0700

    change edit to a post

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ require('dotenv').config();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors({
-  origin: '*',
+  origin: process.env.ALLOW_DOMAIN,
   methods: ['GET','PUT','POST','DELETE','OPTIONS'],
   allowedHeaders: ["Origin, X-Requested-With, Content-Type, Accept"]
-
 }));
 
 app.get('/', (req, res) => {
@@ -36,7 +35,7 @@ app.post('/share', async (req, res) => {
 
 //put /share/:id
 //edit existing share
-app.put('/share/:id', async (req, res) => {
+app.post('/share/edit/:id', async (req, res) => {
   let msg = await repo.Update(req.params.id, req.body);
   res.json(msg);
 });
